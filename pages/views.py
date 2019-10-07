5e2ee243ba9a52f188e8768e9c1ee6762a1ae6db
ddnnss <ifpass112233then>
2019-10-07 11:30:41 +0300

    callback form

--- pages/views.py
+++ pages/views.py
@@ -8,22 +8,27 @@ def index(request):
     return render(request, 'pages/index.html', locals())
 
 def portfolio(request):
+    callbackForm = CallbackForm()
     portfolioItems = PortfolioItem.objects.all()
     filters = Filter.objects.all()
     return render(request, 'pages/portfolio.html', locals())
 
 def portfolio_item(request,nameSlug):
+    callbackForm = CallbackForm()
     portfolioItem = PortfolioItem.objects.get(name_slug=nameSlug)
     images = portfolioItem.porfolioItemImages.all()
     return render(request, 'pages/portfolio_item.html', locals())
 
 def prices(request):
+    callbackForm = CallbackForm()
     return render(request, 'pages/price.html', locals())
 
 def contacts(request):
+    callbackForm = CallbackForm()
     return render(request, 'pages/contacts.html', locals())
 
 def about(request):
+    callbackForm = CallbackForm()
     return render(request, 'pages/about.html', locals())
 
 def callbackForm(request):
